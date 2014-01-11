ec5eafca8112b8bd83fbf245d1177b02722abdbd
Mihail Velikov <mihailvelik@gmail.com>
2014-01-12 00:51:25 +0200

    create new class for statistics results

---
+++ GoCounter/DetailStatisticsViewController.h
@@ -0,0 +1,16 @@
+//
+//  DetailStatisticsViewController.h
+//  GoCounter
+//
+//  Created by mihata on 1/12/14.
+//  Copyright (c) 2014 mihata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailStatisticsViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+
+@end
