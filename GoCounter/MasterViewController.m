9ef7d060955c6906fab26e2923d6da7399142a43
Mihail Velikov <mihailvelik@gmail.com>
2014-01-11 18:32:38 +0200

    remove sending Campaign object to detail view

--- GoCounter/MasterViewController.m
+++ GoCounter/MasterViewController.m
@@ -160,7 +160,6 @@
         NSManagedObject *object = [[self fetchedResultsController] objectAtIndexPath:indexPath];
         [[segue destinationViewController] setDetailItem:object];
         [[segue destinationViewController] setManagedObjectContext:self.managedObjectContext];
-        [[segue destinationViewController] setCampaignObject:sender];
     }
 }
 
