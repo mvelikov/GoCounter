cb8bcd8e9eefaadcf619282663f6b7aa2c662594
Mihail Velikov <mihailvelik@gmail.com>
2014-01-12 00:15:48 +0200

    create tab bar layout and controller for Statistics

---
+++ GoCounter/MasterStatisticsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterStatisticsViewController.h
+//  GoCounter
+//
+//  Created by mihata on 1/12/14.
+//  Copyright (c) 2014 mihata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterStatisticsViewController : UITableViewController
+
+@end
