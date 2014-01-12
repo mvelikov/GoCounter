3659628af5e28ed6a642f61e233b3004d9144a41
Mihail Velikov <mihailvelik@gmail.com>
2014-01-12 23:41:13 +0200

    create helper class

---
+++ GoCounter/Helper.m
@@ -0,0 +1,16 @@
+//
+//  Helper.m
+//  GoCounter
+//
+//  Created by mihata on 1/12/14.
+//  Copyright (c) 2014 mihata. All rights reserved.
+//
+
+#import "Helper.h"
+
+@implementation Helper
+
++ (NSArray*) getCategories {
+    return [[NSArray alloc] initWithObjects:@"none", @"childern", @"adults", @"pensioners", nil];
+}
+@end
