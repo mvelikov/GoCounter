22cbdbdbbaa97e5c28962a50a88ffe36bff46233
Mihail Velikov <mihailvelik@gmail.com>
2014-01-12 13:30:21 +0200

    move methods from parent MasterView class to child class

--- GoCounter/MasterViewController.h
+++ GoCounter/MasterViewController.h
@@ -12,7 +12,7 @@
 
 #import <CoreData/CoreData.h>
 
-@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate, UIAlertViewDelegate>
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
 
 @property (strong, nonatomic) DetailViewController *detailViewController;
 
