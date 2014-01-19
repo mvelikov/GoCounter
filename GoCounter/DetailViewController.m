b3b7d51a1e379908f3857ed7425d245d7dda9486
Mihail Velikov <mihailvelik@gmail.com>
2014-01-19 20:09:39 +0200

    comment 'abort' function before submition'

--- GoCounter/DetailViewController.m
+++ GoCounter/DetailViewController.m
@@ -150,7 +150,7 @@
     NSError *error = nil;
     if (![context save:&error]) {
         NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-        abort();
+//        abort();
     }
     [_campaignObject addCustomersObject:newManagedObject];
     [self.fetchedResultsController performFetch:&error];
@@ -194,7 +194,7 @@
         // Replace this implementation with code to handle the error appropriately.
         // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
 	    NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-	    abort();
+//	    abort();
 	}
     
     return _fetchedResultsController;
